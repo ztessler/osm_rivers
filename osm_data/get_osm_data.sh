2dfa7dd1d7861f8c00fbc1e11f15a41a17f3db70
Zachary Tessler <ztessler@gmail.com>
2018-11-28 10:03:16 -0500

    add india, fix typo

--- osm_data/get_osm_data.sh
+++ osm_data/get_osm_data.sh
@@ -3,10 +3,10 @@
 SERVERPATH="https://download.geofabrik.de/asia"
 SUFFIX="-latest-free.shp.zip"
 
-for COUNTRY in vietnam cambodia thailand
+for COUNTRY in vietnam cambodia thailand india
 do
     FILENAME=${COUNTRY}${SUFFIX}
     wget -N ${SERVERPATH}/${FILENAME}
-    mkdir ${COUNTRY}
+    mkdir -p ${COUNTRY}
     unzip -u -d ${COUNTRY} ${FILENAME}
 done
