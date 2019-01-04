63f6d38fbdfef1bfdc978a5d35c4cac19256a025
Zachary Tessler <ztessler@gmail.com>
2019-01-04 16:52:49 -0500

    small script to run all configurations

---
+++ runall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DELTAs=(Mekong Chao_Phraya Godavari)
+OSMrivers=(vietnam:cambodia thailand india)
+#RESs=(30min 10min 06min 03min 01min)
+RESs=(10min 06min)
+
+for IDX in ${!DELTAs[@]}
+do
+    for RES in ${RESs[@]}
+    do
+        export DELTA=${DELTAs[$IDX]}
+        export STNres=$RES
+        export OSMriver=${OSMrivers[$IDX]}
+
+        # first for each delta as seperate domain
+        export DOMAIN=${DELTAs[$IDX]}
+        scons
+
+        # then for each delta in SSEA domain
+        export DOMAIN="SSEA"
+        scons
+    done
+done
+
+
